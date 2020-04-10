03393e1c03bee0afb12ef69cebecba226642ad13
unknown <nikola.prckovski@gmail.com>
2020-04-10 13:30:13 +0200

    taxReturn method finished

--- Item.java
+++ Item.java
@@ -6,11 +6,32 @@ class Item {
 	int B = 5;
 	int V = 0;
 
-	//TODO constructor
+	class Item(int id, String name, double price){
+	id = id;
+	name = name;
+	price = price;}
 
-	//TODO setters and getters
+	public void setId{
+	this.id=id;}
+
+	public string setName{
+	this.name=name;}
+
+	public void setPrice{
+	this.price=price;}
+
+	public void getId{
+	return id;}
+
+	public string getName{
+	return name;}
+
+	public void getPrice{
+	return price;}
 
 	double taxReturn () {
-		//TODO
-	}
+	float  taxreturn;
+	getPrice * 0.15 = taxreturn;
+	return taxreturn;}
+
 }
